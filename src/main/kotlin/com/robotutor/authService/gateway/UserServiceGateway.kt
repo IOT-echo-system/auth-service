49cd12370ff955d83f6f1bd23527cc49f675682b
Shivam Rajput <raj.shiviraj@gmail.com>
2024-11-06 07:23:24 +0530

    Shivi | Add internal access token.

--- src/main/kotlin/com/robotutor/authService/gateway/UserServiceGateway.kt
+++ src/main/kotlin/com/robotutor/authService/gateway/UserServiceGateway.kt
@@ -3,6 +3,8 @@ package com.robotutor.authService.gateway
 import com.robotutor.authService.config.UserServiceGatewayConfig
 import com.robotutor.authService.gateway.views.UserIdResponse
 import com.robotutor.iot.service.WebClientWrapper
+import com.robotutor.iot.utils.config.AppConfig
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Service
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
@@ -11,7 +13,8 @@ import reactor.core.publisher.Mono
 @Service
 class UserServiceGateway(
     private val userServiceGatewayConfig: UserServiceGatewayConfig,
-    private val webClient: WebClientWrapper
+    private val webClient: WebClientWrapper,
+    private val appConfig: AppConfig
 ) {
     fun getUserId(email: String): Mono<UserIdResponse> {
         val queryParams: MultiValueMap<String, String> = LinkedMultiValueMap()
@@ -20,7 +23,8 @@ class UserServiceGateway(
             baseUrl = userServiceGatewayConfig.baseUrl,
             path = userServiceGatewayConfig.userIdPath,
             queryParams = queryParams,
-            returnType = UserIdResponse::class.java
+            returnType = UserIdResponse::class.java,
+            headers = mapOf(HttpHeaders.AUTHORIZATION to appConfig.internalAccessToken)
         )
     }
 
